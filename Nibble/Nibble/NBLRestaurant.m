a2fd641dcc83959ca5ab94d5862c59b6fa3d56b7
George Chearswat <georgechearswat@Wenbos-MacBook-Air.local>
2013-11-23 16:46:37 -0800

    Added Restaurant Model and Restaurant TableView

---
+++ Nibble/Nibble/NBLRestaurant.m
@@ -0,0 +1,14 @@
+//
+//  NBLRestaurant.m
+//  Nibble
+//
+//  Created by George Chearswat on 11/23/13.
+//  Copyright (c) 2013 NibbleNibble. All rights reserved.
+//
+
+#import "NBLRestaurant.h"
+
+@implementation NBLRestaurant
+
+
+@end
